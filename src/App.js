9ac30264c69678b5c4df3d15d7710cd91d284491
Julio Bertholucci <juliobertholucciferraz@gmail.com>
2024-09-27 15:51:39 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Logo from './componentes/logo'
+import LoginIcon from './imagens/login-alt.svg'
+import CartIcon from './imagens/cart-icon.svg'
+
+const menuItems = ['Sobre Mim', 'Hard Skills', 'Soft Skills', 'Contato'];
+const menuIcons = [LoginIcon, CartIcon] //cria um array com os componentes que queremos exibir
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Logo></Logo> {/* utilização do componente criado e exportado */}
+        <Logo></Logo> {/* utilização do componente criado e exportado */}
+        <ul className='header-menu'>
+          {menuItems.map((item) => (
+            <li className='header-menu-item'><a>{item}</a></li>
+          ))}
+          {menuIcons.map((icon) => ( //utilizamos um map que lê o array e retorna um outro array, exibindo os icones
+            <li className='header-menu-icon'><img src={icon}></img></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -4,7 +4,7 @@ function Logo() { //criar uma função e inserir o código dentro do return
     return(
         <div className='header-logo'>
           <img src={logo} width={'70px'} height={'70px'} alt='logotipo'></img>
-          <p className='header-logo-name'>Julio Bertholucci</p>
+          <p className='header-logo-name'>Julio <strong>Bertholucci</strong></p>
         </div>
     )
 }
