75fe0f76b46cc78ba23e24acc202a1a62c460c4b
Julio Bertholucci <juliobertholucciferraz@gmail.com>
2024-09-27 11:16:59 -0300

    att

---
+++ src/componentes/logo/index.js
@@ -0,0 +1,11 @@
+import logo from '../../imagens/JB.png'
+function Logo() { //criar uma função e inserir o código dentro do return
+    return(
+        <div className='header-logo'>
+          <img src={logo} width={'70px'} height={'70px'} alt='logotipo'></img>
+          <p className='header-logo-name'>Julio Bertholucci</p>
+        </div>
+    )
+}
+
+export default Logo; 
