2c4b73bfb21af5e0e41ed591529557fa7722a89f
Julio Bertholucci <juliobertholucciferraz@gmail.com>
2024-09-29 10:48:54 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
 import Header from './componentes/Header/'
+import styled from 'styled-components';
+{/*Cria uma variável e atribui o styled.div. Styled pode atribuir outras tags como p, h1, header etc*/} 
+const AppContainer = styled.div`    
+    max-width: 100vw; /*view width*/
+    height: 120vh; /*view height*/
+    background-color: #282f32;
+
+  li{
+    list-style: none;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header/>
-    </div>
+    </AppContainer>
   );
 }
 
