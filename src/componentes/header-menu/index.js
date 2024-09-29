897c5041a7f506200a8ffb555a737aef8dc67f6c
Julio Bertholucci <juliobertholucciferraz@gmail.com>
2024-09-29 11:20:06 -0300

    att

--- src/componentes/header-menu/index.js
+++ src/componentes/header-menu/index.js
@@ -1,18 +1,46 @@
 import LoginIcon from '../../imagens/login-alt.svg'
 import CartIcon from '../../imagens/cart-icon.svg'
-import '../header-menu/style.css'
+import styled from 'styled-components';
+
+const HeaderMenuItem = styled.li`
+    text-align: center;
+    align-content: center;
+    justify-content: center;
+    height: 100%;
+    cursor: pointer;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 18px;
+    padding: 0px 25px;
+  
+  :hover{
+    color:#e51e18;
+    transition: 0.5s;
+  }
+`
+const HeaderMenuIcon = styled.li `
+  img{
+    height: 30px;
+    width: 30px;
+    margin: 0px 15px;
+    color:#e51e18;
+    cursor: pointer;
+  }
+`
+const HeaderMenuContainer = styled.ul`
+  display: flex;
+`
 const menuItems = ['Sobre Mim', 'Hard Skills', 'Soft Skills', 'Contato'];
 const menuIcons = [LoginIcon, CartIcon] //cria um array com os componentes que queremos exibir
 function HeaderMenu () {
     return(
-        <ul className='header-menu'>
+        <HeaderMenuContainer>
           {menuItems.map((item) => (
-            <li className='header-menu-item'><a>{item}</a></li>
+            <HeaderMenuItem><a>{item}</a></HeaderMenuItem>
           ))}
           {menuIcons.map((icon) => ( //utilizamos um map que lê o array e retorna um outro array, exibindo os icones
-            <li className='header-menu-icon'><img alt='' src={icon}></img></li>
+            <HeaderMenuIcon><img alt='' src={icon}></img></HeaderMenuIcon>
           ))}
-        </ul>
+        </HeaderMenuContainer>
     )
 }
 
