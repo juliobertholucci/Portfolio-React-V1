741a0e7b5bb957c8ae5c2b73bcf3709c60af3350
Julio Bertholucci <juliobertholucciferraz@gmail.com>
2024-10-03 20:34:39 -0300

    att

--- src/componentes/Contents/contentsData.js
+++ src/componentes/Contents/contentsData.js
@@ -1,7 +1,6 @@
 export const content = [
-    {nome: "Formações", id: 1, conteudo: "aa"},
-    {nome: "Habilidades", id: 2, conteudo: "aa"},
-    {nome: "Portfólio", id: 3, conteudo: "aa"},
-    {nome: "Sobre Mim", id: 4, conteudo: "aa"},
-    {nome: "Contato", id: 5, conteudo: "aa"},
+    {nome: "Formações", id: 1, conteudo: "ETEC Dr. Carolino da Motta e Silva, Alura"},
+    {nome: "Habilidades", id: 2, conteudo: "HTML, CSS, JS, React, Design, SQL"},
+    {nome: "Sobre Mim", id: 4, conteudo: "Tenho 21 anos e conto com 5 anos de experiência em desenvolvimento web, atualmente busco uma vaga de emprego! Gostaria de fazer parte de seu time!!"},
+    {nome: "Contato", id: 5, conteudo: "WhatsApp: (35) 998705890"},
 ]
